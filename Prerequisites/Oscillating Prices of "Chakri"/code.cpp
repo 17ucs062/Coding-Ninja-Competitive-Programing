6b255bf10a7c8796ac11cfcffebcad852cd5b4b2
harsh <32744878+17ucs062@users.noreply.github.com>
2020-03-22 08:09:30 +0530

    Update code.cpp

--- Prerequisites/Oscillating Prices of "Chakri"/code.cpp
+++ Prerequisites/Oscillating Prices of "Chakri"/code.cpp
@@ -19,3 +19,32 @@ int main() {
     cout << max;
     // Write your code here
 }
+/*Alternate O(n) approach
+#include<iostream>
+using namespace std;
+void profit( int arr[],int n) {
+	int minindex=0;
+	int maxprofit=0;
+	int i=1; 
+	while(i<n) {
+		if(arr[i]<arr[minindex]) {
+			minindex=i;
+		}
+		int cp=arr[i]-arr[minindex];
+		if(cp>maxprofit) {
+			maxprofit=cp;
+		}
+		i++;
+	}
+	cout<<maxprofit;
+}
+int main() {
+	int n;
+	cin>>n;
+	int* arr=new int[n];
+	for(int i=0;i<n;i++) {
+		cin>>arr[i];
+	}
+	profit(arr,n);
+}
+*/
