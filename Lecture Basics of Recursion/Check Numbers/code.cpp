8257c6b29cb51adebea11bfa91e1b08376d5f140
harsh <32744878+17ucs062@users.noreply.github.com>
2020-04-09 10:58:48 +0530

    Create code.cpp
    Check Number
    Send Feedback
    Given an array of length N and an integer x, you need to find if x is present in the array or not. Return true or false.
    Do this recursively.
    Input Format :
    Line 1 : An Integer N i.e. size of array
    Line 2 : N integers which are elements of the array, separated by spaces
    Line 3 : Integer x
    Output Format :
    true or false
    Constraints :
    1 <= N <= 10^3
    Sample Input :
    3
    9 8 10
    8
    Sample Output :
    true

---
+++ Lecture Basics of Recursion/Check Numbers/code.cpp
@@ -0,0 +1,41 @@
+bool checkNumber(int input[], int size, int x) {
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Return output and don't print it.
+     Taking input and printing output is handled automatically.
+  */
+    if(size-1 < 0)
+        return false;
+    if(input[size-1] == x)
+        return true;
+    return checkNumber(input,size-1,x);
+
+}
+#include<iostream>
+#include "Solution.h"
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+  
+    int *input = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    int x;
+    
+    cin >> x;
+    
+    if(checkNumber(input, n, x)) {
+        cout << "true" << endl;
+    }
+    else {
+        cout << "false" << endl;
+    }
+}
+
+
