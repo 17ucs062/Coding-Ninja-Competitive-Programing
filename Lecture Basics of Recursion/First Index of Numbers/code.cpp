03b0b359e5b8ff94baccbfafb41d29ccdfbdbd1c
harsh <32744878+17ucs062@users.noreply.github.com>
2020-04-09 10:59:48 +0530

    Create code.cpp
    First Index of Number
    Send Feedback
    Given an array of length N and an integer x, you need to find and return the first index of integer x present in the array. Return -1 if it is not present in the array.
    First index means, the index of first occurrence of x in the input array.
    Do this recursively. Indexing in the array starts from 0.
    Input Format :
    Line 1 : An Integer N i.e. size of array
    Line 2 : N integers which are elements of the array, separated by spaces
    Line 3 : Integer x
    Output Format :
    first index or -1
    Constraints :
    1 <= N <= 10^3
    Sample Input :
    4
    9 8 10 8
    8
    Sample Output :
    1

---
+++ Lecture Basics of Recursion/First Index of Numbers/code.cpp
@@ -0,0 +1,41 @@
+int firstIndex(int input[], int size, int x) {
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Return output and don't print it.
+     Taking input and printing output is handled automatically.
+  */
+    int flag = -1;
+    if(size-1>0)
+        flag = firstIndex(input,size-1,x);
+    if(input[size-1] == x && flag == -1)
+        return size - 1;
+    else if(flag != -1)
+       return flag;
+    else 
+        return -1;
+    
+}
+#include<iostream>
+#include "Solution.h"
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+  
+    int *input = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    int x;
+    
+    cin >> x;
+    
+    cout << firstIndex(input, n, x) << endl;
+
+}
+
+
